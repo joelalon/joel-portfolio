27be68f1d53d13d46db2f09ebfc83f34d3e57747
joelalon <joel_alon@outlook.com>
2024-09-28 04:22:50 -0400

    Updates

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://joelalon.github.io/joel-portfolio",
+  base: "/joel-portfolio/dist/",
 })
