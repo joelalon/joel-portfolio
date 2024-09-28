b000cf1099fe230d1233ba5a52e1382dfe9fcea0
joelalon <joel_alon@outlook.com>
2024-09-28 04:07:36 -0400

    Updates

--- components/About.jsx
+++ components/About.jsx
@@ -7,9 +7,9 @@ export default function About() {
     return (
         <>
             <h1>About Me</h1>
-            <img src="/src/assets/digital_photo.jpg" alt="Joel's Photo" className="about-photo"/>
+            <img src="src/assets/digital_photo.jpg" alt="Joel's Photo" className="about-photo"/>
             <p>Hello! My name is Joel, a passionate Software Developer with expertise in C#, JavaScript, and the MERN stack. Currently pursuing my Software Engineer Technology advance diploma at Centennial College.</p>
-            <a href="/src/assets/resume.pdf" className="download-link" download>Download my Resume (PDF)</a>
+            <a href="src/assets/resume.pdf" className="download-link" download>Download my Resume (PDF)</a>
         </>
     )
 }
--- components/Services.jsx
+++ components/Services.jsx
@@ -9,15 +9,15 @@
             <h1>Services</h1>
             <ul className="services">
                 <li>
-                    <img src="/src/assets/web-development.jpg" alt="Web Development Icon" />
+                    <img src="src/assets/web-development.jpg" alt="Web Development Icon" />
                     <p>Web Development (HTML/CSS, JavaScript, React)</p>
                 </li>
                 <li>
-                    <img src="/src/assets/software-development.jpg" alt="Software Development Icon" />
+                    <img src="src/assets/software-development.jpg" alt="Software Development Icon" />
                     <p>Software Development (C#, Java)</p>
                 </li>
                 <li>
-                    <img src="/src/assets/database-management.png" alt="Database Management Icon" />
+                    <img src="src/assets/database-management.png" alt="Database Management Icon" />
                     <p>Database Management (SQL, MongoDB)</p>
                 </li>
             </ul>
